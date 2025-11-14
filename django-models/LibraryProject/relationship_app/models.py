6aaa5e9b2dd78eb9fe38d5b5c8a1f572e8a9d792
giftbatolu <sekinatoyelami@gmail.com>
2025-11-14 03:35:06 +0100

    model: defina a modeel inside relationship_app/model.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,14 +3,26 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_lenght=250)
 
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_lenght=250)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+    def __str__(self):
+        return f"{self.title} by {self.author.name}"
 
 class Library(models.Model):
     name = models.CharField(max_lenght=250)
     books = models.ManyToManyField(Book, on_delete=models.CASCADE, related_name='library')
 
+    def __str__(self):
+        return self.name
+
 class Librarian(models.Model):
     name = models.CharField(max_lenght=250)
     library = OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} at {self.library.name}"
