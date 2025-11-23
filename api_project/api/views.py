cfb8efb889eac1393e198a42c909cde2bcd7f358
giftbatolu <sekinatoyelami@gmail.com>
2025-11-23 05:18:50 +0100

    feat: Implemetation of authentication system

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,11 +3,14 @@ from rest_framework import generics.ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.viewsets.ModelViewSet import ModelViewSet
+from rest_framework import permissions
 
 class BookList(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
