ea34bede34c9afc7dff0009b53ece468eb56fbe1
giftbatolu <sekinatoyelami@gmail.com>
2025-11-23 04:55:33 +0100

    feat: Define a serializer, create view and urls pattern for the model

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics.ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 
-class BookList(generics.ListAPIView):
+class BookList(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
